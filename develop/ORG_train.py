da20d04d5a9035919c8fbeae0b98c4e056667e33
Sangjoon Han <jpan32@me.com>
2022-12-12 07:51:30 +0900

    Feat: Validation F1-Score

--- develop/ORG_train.py
+++ develop/ORG_train.py
@@ -4,6 +4,7 @@ import errno
 import os.path as osp
 import time
 import math
+import json
 from datetime import datetime, timedelta
 from argparse import ArgumentParser
 
@@ -12,6 +13,8 @@ from torch import cuda
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 from tqdm import tqdm
+from glob import glob
+import cv2
 
 import numpy as np
 import random
@@ -19,6 +22,8 @@ import random
 from east_dataset import EASTDataset
 from dataset import SceneTextDataset
 from model import EAST
+from detect import detect
+from deteval import calc_deteval_metrics
 
 import wandb
 
@@ -32,6 +37,17 @@ def symlink_force(target, link_name):
             os.symlink(target, link_name)
         else:
             raise e
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 def parse_args():
@@ -54,17 +70,24 @@ def parse_args():
     parser.add_argument('--save_interval', type=int, default=5)
     parser.add_argument('--wandb_name', type=str, default='Unnamed Test')
     parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--use_val', type=str2bool, default=True)
+    parser.add_argument('--val_interval', type=int, default=1)
 
     args = parser.parse_args()
 
     if args.input_size % 32 != 0:
         raise ValueError('`input_size` must be a multiple of 32')
 
+    if args.use_val == True and osp.isfile(osp.join(args.data_dir, 'ufo/val.json')) == False:
+        print('Not found: val.json → Please set use_val=False or create val.json!')
+        print('[Warning]: Force reset use_val=False')
+        args.use_val = False
+
     return args
 
 
 def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
-                learning_rate, max_epoch, save_interval, wandb_name, seed):
+                learning_rate, max_epoch, save_interval, wandb_name, seed, use_val, val_interval):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed) # if use multi-GPU
@@ -78,14 +101,35 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
     num_batches = math.ceil(len(dataset) / batch_size)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, worker_init_fn=np.random.seed(seed))
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if use_val:
+        gt_bboxes_dict = dict()
+        transcriptions_dict = dict()
+        with open(osp.join(data_dir, 'ufo/val.json')) as json_file:
+            file_contents = json_file.read()
+            annotation_json = (json.loads(file_contents))['images']
+            for image_name, ann_info in annotation_json.items():
+                gt_bboxes_dict[image_name] = list()
+                transcriptions_dict[image_name] = list()
+                for word in  ann_info['words'].values():
+                    gt_bboxes_dict[image_name].append(word['points'])
+                    transcriptions_dict[image_name].append(word['transcription'])
+        print("Load validation image...")
+        val_images = []
+        for image_name in tqdm(gt_bboxes_dict.keys()):
+            if not osp.isfile(osp.join(data_dir, f'images/{image_name}')):
+                del gt_bboxes_dict[image_name]
+                del transcriptions_dict[image_name]
+                continue
+            val_images.append(cv2.imread(osp.join(data_dir, f'images/{image_name}'))[:, :, ::-1])
+        val_num_batches = math.ceil(len(gt_bboxes_dict) / batch_size)
+
     model = EAST()
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
 
-    model.train()
     for epoch in range(max_epoch):
+        model.train()
         epoch_loss, epoch_start = 0, time.time()
         with tqdm(total=num_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
@@ -105,12 +149,37 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
                     'IoU loss': extra_info['iou_loss']
                 }
                 pbar.set_postfix(val_dict)
-                wandb.log(val_dict)
+                wandb.log({
+                    'Train/Cls loss': extra_info['cls_loss'], 'Train/Angle loss': extra_info['angle_loss'],
+                    'Train/IoU loss': extra_info['iou_loss']
+                })
 
         scheduler.step()
 
         print('Mean loss: {:.4f} | Elapsed time: {}'.format(
             epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
+
+        if use_val and (epoch + 1) % val_interval == 0:
+            val_start = time.time()
+            model.eval()
+            with torch.no_grad():
+                pred_bboxes = list()
+                for step in tqdm(range(val_num_batches)):
+                    images = val_images[batch_size * step:batch_size * (step + 1)]
+                    pred_bboxes.extend(detect(model, images, input_size))
+
+                pred_bboxes_dict = {image_fname:bboxes for image_fname, bboxes in zip(gt_bboxes_dict.keys(), pred_bboxes)}
+                ret = calc_deteval_metrics(pred_bboxes_dict, gt_bboxes_dict, transcriptions_dict, verbose=True)
+                print(" ".join([f"F1: {ret['total']['hmean']:.4f}",
+                                f"Precision: {ret['total']['precision']:.4f}",
+                                f"Recall: {ret['total']['recall']:.4f}",
+                                f"| Elapsed time: {timedelta(seconds=time.time() - val_start)}"
+                              ]))
+
+                wandb.log({
+                    'Val/Precision': ret['total']['precision'], 'Val/Recall': ret['total']['recall'],
+                    'Val/F1': ret['total']['hmean']
+                })
 
         if (epoch + 1) % save_interval == 0:
             if not osp.exists(model_dir):
