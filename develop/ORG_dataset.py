9d8f566e318c879d998557e8734c4a1da200e615
Sangjoon Han <jpan32@me.com>
2022-12-14 02:13:09 +0900

    Hotfix

--- develop/ORG_dataset.py
+++ develop/ORG_dataset.py
@@ -382,7 +382,7 @@ class SceneTextDataset(Dataset):
         if self.color_jitter:
             funcs.append(A.ColorJitter(0.5, 0.5, 0.5, 0.25))
         if self.normalize:
-            funcs.append(A.Normalize(mean=(0.485, 0.456, 0.406, std=(0.229, 0.224, 0.225)))
+            funcs.append(A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)))
         transform = A.Compose(funcs)
 
         image = transform(image=image)['image']
