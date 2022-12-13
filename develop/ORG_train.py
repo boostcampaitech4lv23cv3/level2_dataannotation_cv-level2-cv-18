acbe224e1caeebc49d9a7ad10ca029f4a12763a5
ENTER YOUR NAME <ENTER YOUR EMAIL>
2022-12-13 21:01:38 +0900

    FEAT:new_dataset docstring add

--- develop/ORG_train.py
+++ develop/ORG_train.py
@@ -48,7 +48,7 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
-        
+
 
 def parse_args():
     parser = ArgumentParser()
@@ -64,7 +64,7 @@ def parse_args():
 
     parser.add_argument('--image_size', type=int, default=1024)
     parser.add_argument('--input_size', type=int, default=512)
-    parser.add_argument('--batch_size', type=int, default=12)
+    parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--max_epoch', type=int, default=200)
     parser.add_argument('--save_interval', type=int, default=5)
